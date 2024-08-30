bbaa86437f97f1b039dc4786b4d47fa752ebfc17
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-30 14:40:32 +0700

    using optional query graphql

--- graphql/models/User.ts
+++ graphql/models/User.ts
@@ -1,4 +1,5 @@
 import { builder } from "../builder";
+import prisma from "../../libs/prisma"
 
 builder.prismaObject("User", {
     fields: t => ({
@@ -7,10 +8,23 @@ builder.prismaObject("User", {
         messages: t.relation("messages")
     })
 });
-
 // Example without using pothos
 /* type User {
     id: ID!
     messages: [Message!]!
     name: String!
+} */
+
+builder.queryField("users", (t) =>
+    t.prismaField({
+        type: ["User"],
+        resolve: async (query) => {
+            return prisma.user.findMany({ ...query });
+        },
+    })
+);
+
+// Example without using pothos
+/* type Query {
+    users: [User!]!
 } */
