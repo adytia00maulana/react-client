faf499abdae76037c765e0ee7d8eb67c7ed9d0fa
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-30 10:32:35 +0700

    add data and seed

---
+++ prisma/seed/data/usersAndMessages.ts
@@ -0,0 +1,41 @@
+export const usersAndMessages = [
+    {
+        name: "Jack",
+        messages: {
+            create: [
+                {
+                    body: "A Note for Jack",
+                },
+                {
+                    body: "Another note for Jack",
+                },
+            ],
+        },
+    },
+    {
+        name: "Ryan",
+        messages: {
+            create: [
+                {
+                    body: "A Note for Ryan",
+                },
+                {
+                    body: "Another note for Ryan",
+                },
+            ],
+        },
+    },
+    {
+        name: "Adam",
+        messages: {
+            create: [
+                {
+                    body: "A Note for Adam",
+                },
+                {
+                    body: "Another note for Adam",
+                },
+            ],
+        },
+    }
+];
--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         }]
     }]
     return (
-        // <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">{users.map((user, i) => <UserDisplay user={user} key={i}/>)}</div>
+        // <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">{usersAndMessages.map((user, i) => <UserDisplay user={user} key={i}/>)}</div>
         <div className="bg-zinc-800 flex-col min-h-screen w-full flex items-center justify-center">{users.map((user, i) => <UserDisplay user={user} key={i}/>)}</div>
     )
 }
