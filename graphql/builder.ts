e88376fa8fd4a9be4cbc5602852c650f6c16f51b
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-30 13:55:40 +0700

    Create a reusable instance of Prisma Client

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -1,13 +1,22 @@
-import SchemaBuilder from "@pothos/core";
 // 1
+import SchemaBuilder from "@pothos/core";
 import { DateResolver } from "graphql-scalars";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import type PrismaTypes from "@pothos/plugin-prisma/generated"
+import prisma from "../libs/prisma";
 
 // 2
 export const builder = new SchemaBuilder<{
     Scalars: {
         Date: { Input: Date; Output: Date };
     };
-}>({});
+    PrismaTypes: PrismaTypes;
+}>({
+    plugins: [PrismaPlugin],
+    prisma: {
+        client: prisma,
+    },
+});
 
 // 3
 builder.addScalarType("Date", DateResolver, {});
